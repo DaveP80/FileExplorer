799d28dcccb127dff20ca3676bed0f76800abb9b
DaveP80 <davidpaq1@gmail.com>
2022-09-11 21:18:23 -0400

    "collections"

--- src/main/java/RegexApp.java
+++ src/main/java/RegexApp.java
@@ -1,31 +1,32 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexApp { //returns result as array, with matcher as elements in the array
     public static void main(String[] args) throws IOException {
 
-        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\david\\Desktop\\ResultFile")); //use path to your file created from pwsl script
-        final List<String> names = new ArrayList<>();
+        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\yourName\\")); //use path to your file created from pwsl script
+        List<String> names = new ArrayList<>();
+
         for (String line : lines) {
-            Pattern pattern = Pattern.compile("(.*)sudo(.*)");
+            Pattern pattern = Pattern.compile("(.*)example(.*)");
             Matcher matcher = pattern.matcher(line);
             while (matcher.find()) {
                 String name = matcher.group(0);
                 name = name.substring(0);
                 names.add(name);
+                names.sort(Comparator.comparingInt(String::length));
             }
         }
-        System.out.println("Paths found: " + names.toArray().length);
-        for (int i = 0; i < names.toArray().length; i++) {
-            System.out.println(names.toArray()[i]);
-        }
-//        System.out.println(Arrays.toString(names.toArray()));
-    }
+        System.out.println("Paths found: " + names.size());
 
+        for (String i : names) {
+            System.out.println(i);
+        }
+    }
 }
+
+
