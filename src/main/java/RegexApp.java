0128e46946de3b15110b31223e8856a0128cf948
DaveP80 <davidpaq1@gmail.com>
2022-09-11 20:02:26 -0400

    "collections"

--- src/main/java/RegexApp.java
+++ src/main/java/RegexApp.java
@@ -10,14 +10,14 @@ import java.util.regex.Pattern;
 public class RegexApp { //returns result as array, with matcher as elements in the array
     public static void main(String[] args) throws IOException {
 
-        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\yourName\\")); //use path to your file created from pwsl script
+        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\david\\Desktop\\ResultFile")); //use path to your file created from pwsl script
         final List<String> names = new ArrayList<>();
         for (String line : lines) {
-            Pattern pattern = Pattern.compile("(.*)example(.*)");
+            Pattern pattern = Pattern.compile("(.*)sudo(.*)");
             Matcher matcher = pattern.matcher(line);
             while (matcher.find()) {
                 String name = matcher.group(0);
-                name = name.substring(1);
+                name = name.substring(0);
                 names.add(name);
             }
         }
